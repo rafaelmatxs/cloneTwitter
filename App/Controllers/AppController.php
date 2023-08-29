bfdb8a9176a38dfe9005720c37f9909345b706ce
rafaelmatxs <rafaelbragaodematos@hotmail.com>
2023-08-29 04:10:00 -0300

    TwitterCloneCompleto

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -19,6 +19,14 @@ class AppController extends Action {
 
         $this->view->tweets = $tweets;
 
+        $usuario = Container::getModel('Usuario');
+        $usuario->__set('id', $_SESSION['id']);
+
+        $this->view->info_usuario = $usuario->getInfoUsuario();
+        $this->view->total_tweets = $usuario->getTotalTweets();
+        $this->view->total_seguindo = $usuario->getTotalSeguindo();
+        $this->view->total_seguidores = $usuario->getTotalSeguidores();
+
         $this->render('timeline');
     }
 
@@ -60,6 +68,15 @@ class AppController extends Action {
 
         $this->view->usuarios = $usuarios;
 
+        // adicionei agora
+        $usuario = Container::getModel('Usuario');
+        $usuario->__set('id', $_SESSION['id']);
+        $this->view->info_usuario = $usuario->getInfoUsuario();
+        $this->view->total_tweets = $usuario->getTotalTweets();
+        $this->view->total_seguindo = $usuario->getTotalSeguindo();
+        $this->view->total_seguidores = $usuario->getTotalSeguidores();
+        // /adicionei agora
+
         $this->render('quemSeguir');
     }
 
@@ -79,6 +96,14 @@ class AppController extends Action {
         }
 
         header('Location: /quem_seguir');
+    }
+
+    public function removerTweet() {
+        $this->validaAutenticacao();
+
+        $tweet = Container::getModel('Tweet');
+        $tweet->__set('id', $_GET['id']);
+        $tweet->deletar();      
     }
 
 }
--- App/Models/Usuario.php
+++ App/Models/Usuario.php
@@ -123,6 +123,46 @@ class Usuario extends Model {
 
         return true;
     } 
+
+    //Informações usuario
+    public function getInfoUsuario() {
+        $query = "select nome from usuarios where id = :id_usuario";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(':id_usuario', $this->__get('id'));
+        $stmt->execute();
+
+        return $stmt->fetch(\PDO::FETCH_ASSOC);
+    }
+    
+    //total tweets
+    public function getTotalTweets() {
+        $query = "select count(*) as total_tweet from tweets where id_usuario = :id_usuario";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(':id_usuario', $this->__get('id'));
+        $stmt->execute();
+
+        return $stmt->fetch(\PDO::FETCH_ASSOC);
+    }
+
+    //total seguindo
+    public function getTotalSeguindo() {
+        $query = "select count(*) as total_seguindo from usuarios_seguidores where id_usuario = :id_usuario";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(':id_usuario', $this->__get('id'));
+        $stmt->execute();
+
+        return $stmt->fetch(\PDO::FETCH_ASSOC);
+    }
+
+    //total seguidores
+    public function getTotalSeguidores() {
+        $query = "select count(*) as total_seguidores from usuarios_seguidores where id_usuario_seguindo = :id_usuario";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(':id_usuario', $this->__get('id'));
+        $stmt->execute();
+
+        return $stmt->fetch(\PDO::FETCH_ASSOC);
+    }
 }
 
 ?>
